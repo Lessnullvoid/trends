9f54af83c59e4112cb077a35608558dfa8585c1d
Microhom <microhom@gmail.com>
2017-06-19 19:32:42 -0300

    comments

--- main.py
+++ main.py
@@ -1,86 +1,93 @@
+# USAGE
+# python motion_detector.py
+# python motion_detector.py --video videos/example_01.mp4
 
-#llamar librerias
+# import the necessary packages
 import argparse
 import datetime
 import imutils
 import time
 import cv2
 
-
-# construir el argumento para parsear
+# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to video")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="area minima")
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
-# si el argumento es none, estonces leemos desde una webcam
+# if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
-    camera = cv2.VideoCapture(0)
-    time.sleep(0.25)
+	camera = cv2.VideoCapture(0)
+	time.sleep(0.25)
 
-#si es else leemos dese un archivo
+# otherwise, we are reading from a video file
 else:
-    camera = cv2.VideoCapture(args["video"])
+	camera = cv2.VideoCapture(args["video"])
 
-#iniciarlizar el primer frame
+# initialize the first frame in the video stream
 firstFrame = None
 
-#Loop del video
+# loop over the frames of the video
 while True:
+	# grab the current frame and initialize the occupied/unoccupied
+	# text
+	(grabbed, frame) = camera.read()
+	text = "Unoccupied"
 
-    (grabbed, frame) = camera.read()
-    text = "OFF"
+	# if the frame could not be grabbed, then we have reached the end
+	# of the video
+	if not grabbed:
+		break
 
-    if not grabbed:
-        break
+	# resize the frame, convert it to grayscale, and blur it
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
+	# if the first frame is None, initialize it
+	if firstFrame is None:
+		firstFrame = gray
+		continue
 
-    # resize frame y convertir a escala de grises y aplicar blur
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+	# compute the absolute difference between the current frame and
+	# first frame
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    # si el primer frame es none iniciarlizar
-    if firstFrame is None:
-        firstFrame = gray
-        continue
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
 
-    #computar la diferencia absoluta entre el frame actual y primero.
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    tresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY[1])
+	# loop over the contours
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < args["min_area"]:
+			continue
 
-    #dilatar la imagen y encontrar contours
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                 cv2.CHAIN_APPROX_SIMPLE)
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Occupied"
 
-    #loop en lo contours
-    for c in cnts:
-        #si es muy pequeno
-        if cv2.contourArea(c) < args['main_area']:
-            continue
+	# draw the text and timestamp on the frame
+	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-    #computar el bounding box y actualizar el estado del detector
-    (x, y, w, h) = cv2.boundingRect(c)
-    cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255,0), 2)
-    text = "ON"
+	# show the frame and record if the user presses a key
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
 
-    #dibujar los estados en las pantalla
-    cv2.putText(frame, "estado del sensor: {}".format(text), (10,20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255) 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%P"),
-        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
 
-    #mostrar la imagen y grabarla con orden de tecla
-
-    cv2.imshow("Feed", frame)
-    cv2.imshow("Thresh" thresh)
-    cv2.imshow("Frame Delta" frameDelta)
-    key = cv2.waitKey(1) ^ 0xFF
-
-    if key == ord("q"):
-        break
-
-
-    camera.release()
-    cv2.destroyAllWindows
+# cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
