16ae01df69c2c2399f8f5181be7002177c547101
Microhom <microhom@gmail.com>
2017-06-21 00:37:54 -0300

    added piglet window and label

--- main.py
+++ main.py
@@ -11,39 +11,58 @@ import math
 import cv2
 import OSC
 import numpy
+import pyglet
 from pytrends.request import TrendReq
 
+
 # enter your own credentials
-google_username = "microhom@gmail.com"
+google_username = "xxx"
 google_password = "xxx"
 path = ""
 
 #Login to google
-pytrend = TrendReq(google_username, google_password, custom_useragent="RenzoTrend Script")
+pytrend = TrendReq(google_username,
+                        google_password,
+                        custom_useragent="RenzoTrend Script")
 #capture API tokens
 pytrend.build_payload(kw_list=['temblor', 'heartquake', 'terremoto'])
-
 
 # osc init
 send_addr = "192.168.0.20", 57120
 cOsc = OSC.OSCClient()
 cOsc.connect(send_addr)
 
+#window and graphics
+window = pyglet.window.Window()
+label = pyglet.text.Label('PRINT TRENDS',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area",
+                        type=int,
+                        default=500,
+                        help="minimum area size")
+
 args = vars(ap.parse_args())
 
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
 	camera = cv2.VideoCapture(0)
 	time.sleep(0.25)
-
 # otherwise, we are reading from a video file
 else:
 	camera = cv2.VideoCapture(args["video"])
-
 # initialize the first frame in the video stream
 firstFrame = None
 
@@ -78,8 +97,9 @@ while True:
 	# dilate the thresholded image to fill in holes, then find contours
 	# on thresholded image
 	thresh = cv2.dilate(thresh, None, iterations=2)
-	(cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
+	(cnts, _) = cv2.findContours(thresh.copy(),
+                        cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
 
 	# loop over the contours
 	for c in cnts:
@@ -92,19 +112,12 @@ while True:
 		(x, y, w, h) = cv2.boundingRect(c)
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
 		text = "ON"
-
         # send osc message to start_sample
         msg = OSC.OSCMessage()
         msg.setAddress("/1")
         msg.append(1)
         cOsc.send(msg)
 
-        time.sleep(5)
-
-        msg = OSC.OSCMessage()
-        msg.setAddress("/1")
-        msg.append(0)
-        cOsc.send(msg)
 
 	# draw the text and timestamp on the frame
 	cv2.putText(frame, "Estado del sensor: {}".format(text), (10, 20),
@@ -118,9 +131,14 @@ while True:
 	#cv2.imshow("Frame Delta", frameDelta)
 	key = cv2.waitKey(1) & 0xFF
 
+
+
+
 	# if the `q` key is pressed, break from the lop
 	if key == ord("q"):
 		break
+pyglet.app.run()
+
 
 # cleanup the camera and close any open windows
 camera.release()
