b143537e51781346cc7989c8ce19a034cb2938c9
Microhom <microhom@gmail.com>
2017-07-24 16:43:56 +0200

    update

--- trends_B.py
+++ trends_B.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
 	args = vars(ap.parse_args())
 
 	# osc
-	recv_addr = "192.168.1.38", 10001
+	recv_addr = "127.0.0.1", 10001
 	s = OSC.OSCServer(recv_addr)
 	s.addMsgHandler('/cell', cell_callback)
 	st = threading.Thread(target=s.serve_forever)
