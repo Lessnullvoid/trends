928a2fd33be177896f4b115ae6e254b71088c1b9
Microhom <microhom@gmail.com>
2017-09-12 22:01:45 -0300

    update

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@
 print("******************************************************")
 print("* Inicializando la raspberry:                        *")
 print("* Comprobando librerias:                             *")
-print("* finalinzando inicializacion:                       *")
+print("* Iniciando programa TRENDS****                      *")
 print("******************************************************")
