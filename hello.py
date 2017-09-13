8297cced83b32816269e6b35b9f7748b0292cc3a
Microhom <microhom@gmail.com>
2017-09-12 21:52:43 -0300

    inicializar

---
+++ hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+print("******************************************************")
+print("* Inicializando la raspberry:                        *")
+print("* Comprobando librerias:                             *")
+print("* finalinzando inicialización:                       *")
+print("******************************************************")
