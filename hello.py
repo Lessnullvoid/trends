981abfc4ed55397e445d2232658e4223e536ea93
Microhom <microhom@gmail.com>
2017-09-14 11:18:30 -0300

    update

--- hello.py
+++ hello.py
@@ -2,5 +2,7 @@
 print("******************************************************")
 print("* Inicializando la raspberry:                        *")
 print("* Comprobando librerias:                             *")
-print("* Raspberry lista para iniciar scripts:               *")
+print("* Raspberry lista para iniciar scripts:              *")
+print("* Codigo por STUDIO MATERIAL:                        *")
+print("* https://github.com/Lessnullvoid/trends             *")
 print("******************************************************")
