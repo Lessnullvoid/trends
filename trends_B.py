847743b5b0755606d84eca31310ab3d8889787f1
Microhom <microhom@gmail.com>
2017-10-05 12:45:37 -0500

    update font

--- trends_B.py
+++ trends_B.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
 	s = pygame.Surface((disp_w, 110))
 	ss = pygame.Surface((disp_w, disp_h))
 
-	font = pygame.font.Font("arial.ttf", 180)
+	font = pygame.font.Font("arial.ttf", 150)
 	text = '[0FF]'
 	size = font.size(text)
 	c_w = 250, 240, 230
